f92adf0dbbb1290a6e08a412804c48e87c06d512
Pratik Bhattacharjee <pratikbhattacharjee09@gmail.com>
2024-03-05 13:28:38 +0800

    Dependencies added

---
+++ src/main/java/com/pratik/Jwtspringsecurityrestapi/JwtSpringSecurityRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.pratik.Jwtspringsecurityrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringSecurityRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSpringSecurityRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratik/Jwtspringsecurityrestapi/JwtSpringSecurityRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik.Jwtspringsecurityrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringSecurityRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
