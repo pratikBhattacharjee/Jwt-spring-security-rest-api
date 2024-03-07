3545450d9c04be9c211e183b6dd7247a34e3c0a2
Pratik B <pratikbhattacharjee09@gmail.com>
2024-03-07 14:59:16 +0800

    Added secured endpoint for testing

---
+++ src/main/java/com/pratik/Jwtspringsecurityrestapi/controllers/DemoControllers.java
@@ -0,0 +1,15 @@
+package com.pratik.Jwtspringsecurityrestapi.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/secured")
+public class DemoControllers {
+    @GetMapping
+    public ResponseEntity<String> secured() {
+        return ResponseEntity.ok("This is a secured endpoint");
+    }
+}
